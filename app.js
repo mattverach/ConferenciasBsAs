3ca443ca3e77b39d11ba45127d4d9ef3fc4063ea
Matías Vera <matias_vera@outlook.com>
2021-10-23 23:54:52 -0300

    FixedBugs
    Fixed some bugs and added an alert when amount of tickets was over 10

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let valorTicket = 200;
 let alertEl = document.getElementById("alert-el")
-
+let alertDiv = document.getElementById("alertcont")
+document.getElementById("inputcantidad").value = 1
 
 function resumen() {
     let cantidadEl = Math.ceil(document.getElementById("inputcantidad").value)
@@ -10,6 +11,17 @@ function resumen() {
     if (cantidadEl < 0) {
         cantidadEl = 1
         document.getElementById("inputcantidad").value = cantidadEl 
+    } else if (cantidadEl > 10) {
+        cantidadEl = 10
+        document.getElementById("inputcantidad").value = cantidadEl 
+        var tag = document.createElement("div")
+        tag.classList.add('alert-danger')
+        tag.classList.add('alert')
+        let text = document.createTextNode("Cantidad máxima de entradas por persona: 10");
+        tag.appendChild(text);
+        alertDiv.appendChild(tag);
+    } else {
+        alertDiv.removeChild(alertDiv.childNodes[2])
     }
 
     if (categoriaEl == "estudiante") {
